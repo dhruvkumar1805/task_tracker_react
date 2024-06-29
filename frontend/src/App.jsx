375ad1b5d5cfeb0cbc32466312476d22d6b844d9
dhruvkumar1805 <dhruvkumar1805@gmail.com>
2024-06-29 12:27:41 +0530

    feat: Redirect logged-in users from login/signup pages
    Signed-off-by: dhruvkumar1805 <dhruvkumar1805@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,41 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Task from "./components/Task";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Task />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={isLoggedIn ? <Task /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={isLoggedIn ? <Navigate to="/" /> : <SignUp />}
+        />
+        <Route
+          path="/login"
+          element={isLoggedIn ? <Navigate to="/" /> : <Login />}
+        />
       </Routes>
     </Router>
   );
